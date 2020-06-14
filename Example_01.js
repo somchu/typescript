afd38d534ab50cc140812a636a878dd265992c48
[somchu] <[somchu29@gmail.com]>
2020-06-14 17:12:24 +0700

    Example_01.ts commit

---
+++ Example_01.js
@@ -0,0 +1,5 @@
+var html = document.createElement('div');
+var testData = 10;
+console.log(testData);
+html.textContent = 'The result is: ' + testData;
+document.body.appendChild(html);
---
+++ Example_01.ts
@@ -0,0 +1,5 @@
+let html=document.createElement('div');
+const testData: number = 10;
+console.log (testData);
+html.textContent = 'The result is: '+ testData;
+document.body.appendChild(html);
